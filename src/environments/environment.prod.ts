3609a85826bb8adfed819d201eccd73442b28857
Robert Rice <rarice@g.cofc.edu>
2018-03-28 09:35:25 -0600

    setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+   apiKey: "AIzaSyDxCOYJH9_Rh9RyDFA_WhR4hTFIghmG6Kc",
+   authDomain: "clientpanelprod-c7a54.firebaseapp.com",
+   databaseURL: "https://clientpanelprod-c7a54.firebaseio.com",
+   projectId: "clientpanelprod-c7a54",
+   storageBucket: "clientpanelprod-c7a54.appspot.com",
+   messagingSenderId: "955977727478"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+   apiKey: "AIzaSyDxCOYJH9_Rh9RyDFA_WhR4hTFIghmG6Kc",
+   authDomain: "clientpanelprod-c7a54.firebaseapp.com",
+   databaseURL: "https://clientpanelprod-c7a54.firebaseio.com",
+   projectId: "clientpanelprod-c7a54",
+   storageBucket: "clientpanelprod-c7a54.appspot.com",
+   messagingSenderId: "955977727478"
+  }
 };

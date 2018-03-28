8827cc7492ddf1f587d0099df04f1a13290d17f5
Robert Rice <rarice4@gmail.com>
2018-03-28 08:22:48 -0600

    start router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ClientDetailsComponent } from './components/client-details/client-detai
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { SettingsComponent } from './components/settings/settings.component';
 
 
 @NgModule({
@@ -27,10 +29,12 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     ClientDetailsComponent,
     LoginComponent,
     RegisterComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

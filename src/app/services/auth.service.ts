e8994f2bbaa49d8dbd8c470ab6dbec9ad160a168
Robert Rice <rarice@g.cofc.edu>
2018-04-06 17:53:23 -0600

    register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,4 +24,11 @@ export class AuthService {
     this.afAuth.auth.signOut();
   }
 
+  register(email:string, password:string){
+    return new Promise((resolve,reject)=>{
+      this.afAuth.auth.createUserWithEmailAndPassword(email,password)
+      .then(userData=>resolve(userData),err=>reject(err))
+    });
+  }
+
 }
